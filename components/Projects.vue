259e19fa628a18594280558c1256adc3c690b76e
BagasCaturS <bagascatursantoso@gmail.com>
2024-07-30 23:15:50 +0700

    Added new icones dependency and fixed or imporved some files

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,59 +1,77 @@
 <template>
-    <div class="flex flex-col items-center gap-4">
-        <div class="mx-auto flex justify-center">
+    <div class="container mx-auto p-8 rounded-lg">
 
-            <div class=" container p-4 bg-base-200 w-3/5 rounded-md">
-                <div class="grid grid-cols-2 justify-between">
-                    <!-- I will fix this later -->
-                    <div class="p-3 w-fit flex flex-col justify-center border border-accent rounded-md text-justify">
-                        <h1>Data Scientist</h1>
-                        <p class="mt-1">Telkom University, Bandung, Indonesia</p>
-                    </div>
-                    <div class="flex justify-end">
+        <div class="flex flex-col items-center gap-4">
+            <div class="mx-auto flex justify-center">
 
-                        <div class="p-3 border border-accent rounded-md w-fit h-fit">
-                            <h1>02-2024</h1>
+                <div class=" container p-4 bg-base-200 w-3/5 rounded-md">
+                    <div class="grid grid-cols-2 justify-between">
+                        <!-- I will fix this later -->
+                        <div
+                            class="p-3 w-fit flex flex-col justify-center border border-accent rounded-md text-justify">
+                            <h1>Internship as a <span class="text-warning">Data Scientist</span></h1>
+                            <p class="mt-1">Telkom University, Bandung, Indonesia</p>
+                        </div>
+                        <div class="flex justify-end">
+
+                            <div class="p-3 border border-accent rounded-md w-fit h-fit ">
+                                <h1>02-2024 | 08-2024</h1>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="ml-4 mt-4 text-justify">
-                    <li>As a Data Scientist, I am responsible for creating
-                        machine learning models using the Python
-                        programming language, <span class="ml-6">with the goal of
+                    <div class="ml-4 mt-4 text-justify">
+                        <div class="flex">
+                            <li></li>
+
+                            As a Data Scientist, I am responsible for creating
+                            machine learning models using the Python
+                            programming language, with the goal of
                             identifying various types of waste, such as plastic
-                            bottles, glass bottles, masks, and beverage cans;</span>
-                    </li>
-                    <li>Responsible for collecting the dataset needed for
-                        training the machine learning model. This dataset
-                        consists of a total <span class="ml-6">of 2.200 images encompassing
-                            various types of waste to be identified by the
-                            model;</span>
-                    </li>
-                    <li>Selecting the machine learning algorithms to be
-                        used in building the model;
-                    </li>
+                            bottles, glass bottles, masks, and beverage cans;
+
+                        </div>
+                        <div class="flex ">
+                            <li></li>
+                            <p>
+                                Responsible for collecting the dataset needed for
+                                training the machine learning model. This dataset
+                                consists of a total of 2.200 images encompassing
+                                various types of waste to be identified by the
+                                model;
+                            </p>
+                        </div>
+                        <div class="flex">
+
+                            <li>
+                            </li>
+                            <p>
+                                Selecting the machine learning algorithms to be
+                                used in building the model;
+                            </p>
+                        </div>
+                    </div>
+
+
                 </div>
+                <div class="border-l mx-3 border-2 border-warning"></div>
+                <div class="flex flex-col">
 
+                    <div class=" text-center border p-2 mb-4 rounded-md border-accent">
+                        <h1 class="text-2xl ">Techs i used</h1>
+                    </div>
 
+                    <div class="flex mx-4 gap-4">
+                        <Icon name="simple-icons:github" size="4rem" />
+                        <Icon name="simple-icons:googlecolab" size="4rem" />
+                        <Icon name="simple-icons:tensorflow" size="4rem" />
+                        <Icon name="simple-icons:python" size="4rem" />
+                    </div>
+                </div>
             </div>
-            <div class="border-l mx-3 border-2 border-warning"></div>
-            <div class="flex flex-col">
 
-                <div class="text-center">
-                    <h1 class="text-2xl border p-2 mb-4 rounded-md border-accent">Techs i used</h1>
-                </div>
-
-                <div class="flex mx-4 gap-4">
-                    <Icon name="simple-icons:github" size="4rem" />
-                    <Icon name="simple-icons:googlecolab" size="4rem" />
-                    <Icon name="simple-icons:tensorflow" size="4rem" />
-                    <Icon name="simple-icons:python" size="4rem" />
-                </div>
+            <div class=" container p-4 bg-base-200 w-3/5 rounded-md">
+                <h1>sdjsdjsj</h1>
             </div>
-        </div>
-
-        <div class=" container p-4 bg-base-200 w-3/5 rounded-md">
-            <h1>sdjsdjsj</h1>
         </div>
     </div>
 </template>
